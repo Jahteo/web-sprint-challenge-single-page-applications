704b475eeff10e2d3698b09886606db307e96035
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-21 13:16:27 -0700

    validateChange still broken, but in a different way now. added basic error display under name input for when it actually works.

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -5,27 +5,60 @@ import * as yup from "yup"
 export default function Pizza () {
     const initialOrder = {name: "", size: "", toppings: {cheese: "", anchovies: "", pepperoni: "", balsamic: ""}, instructions: ""}
     const [order, setOrder] = useState({initialOrder})
-    const [errors, setErrors] = useState()
+    const [errors, setErrors] = useState({name: "", size: "", toppings: {cheese: "", anchovies: "", pepperoni: "", balsamic: ""}, instructions: ""})
 
+    //mine is erroring out for some reason
+    const formSchema = yup.object().shape({
+        name: yup.string().min(2, "at least 2 letters please").required("Name is a required field.")
+    })
+    //Currenlty doing something very strange. sending the order object back in my errors & breaking when I input into any field other than name(which I think has to do with only having name in my schema).
+    const validateChange = (e) => {
+        yup.reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then(valid => {
+                setErrors({...errors, [e.target.name]: ""});
+            })
+            .catch(err => {
+                setErrors({...errors, [e.target.name]: err.errors[0]});
+                console.log("errors:", errors)
+            });
+    }
+//copied stright from notes for reference, but still having problems.
     // const formSchema = yup.object().shape({
-    //     name: yup.string().min(2, "at least 2 letters please").required("Name is a required field."),
-    // })
-    // const validateChange = (e) => {
-    //     yup.reach(formSchema, e.target.name)
-    //         .validate(e.target.name === "terms" ? e.target.checked : e.target.value)
-    //         .then(valid => {
-    //             setErrors({...errors, [e.target.name]: ""});
-    //         })
-    //         .catch(err => {
-    //             setErrors({...errors, [e.target.name]: err.errors[0]});
+    //     name: yup.string().required("Name is a required field."),
+    //     email: yup
+    //       .string()
+    //       .email("Must be a valid email address.")
+    //       .required("Must include email address."),
+    //     terms: yup.boolean().oneOf([true], "please agree to terms of use"),
+    //     positions: yup
+    //         .string()
+    //         .oneOf(["Newsletter", "Yard Work", "Administrative Work", "Tabling"]), // value must be one of the values in the array, otherwise throws error
+    //     motivation: yup.string().required("must include why you'd like to join")
+    //   });
+    // const validateChange = e => {
+    //     yup
+    //       .reach(formSchema, e.target.name)
+    //       .validate(e.target.name === "terms" ? e.target.checked : e.target.value) // value in input
+    //       .then(valid => {
+    //         setErrors({ // if there was an error, clear it since value is valid
+    //           ...errors,
+    //           [e.target.name]: ""
     //         });
-    //     };
-    // }
+    //       })
+    //       .catch(err => {
+    //         setErrors({ // add error by name of input since value breaks validation
+    //           ...errors,
+    //           [e.target.name]: err.errors[0]
+    //         });
+    //       });
+    //   };
 
     const handleChanges = (e) => {
+        e.persist()
         const newOrderData = {...order, [e.target.name]: e.target.type === "checkbox" ? e.target.checked : e.target.value}
         console.log(newOrderData)
-        // validateChange ();
+        // validateChange(e)
         setOrder(newOrderData)
     }
     const submitOrder = (e) => {
@@ -39,6 +72,7 @@ export default function Pizza () {
             <form onSubmit={submitOrder}>
                 <label htmlFor="name" /> Name <p className="required">Required</p>
                     <input type="text" id="name" name="name" onChange={handleChanges} value={order.name} placeholder="Enter name" data-cy="name"></input>
+                    {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
                 <br />
                 <label htmlFor="size" /> Choose a Pizza Size <p className="required">Required</p>
                     <select id="size" onChange={handleChanges} name="size" value={order.size} data-cy="size">
