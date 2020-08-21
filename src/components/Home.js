1760598780ac6f70b731eddcdce1aef42ab48088
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-21 09:24:00 -0700

    added useHistory() to Home.js, fleshed out button functionality.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,23 @@
 import React from "react"
+import { useHistory } from "react-router-dom"
+import { Link, Route, Switch } from "react-router-dom"
 
 export default function Home () {
+    const history = useHistory()
+    const routeToPizza = () => {
+        history.push("/pizza")
+    }
+
 
     return (
         <div>
-            Home Screen: Pizza Button
-            {/* <img src="../assets/pizza_delivery_scooter.img" alt="Pizza delivery scooter"></img> */}
+            <h1>Welcome to Lambda Eats</h1>
+            <h2>Because great coders need the best food service ISA's and RCI's can offer!</h2>
+            {/* is the routeToPizza somthing biult into Link? idk why to use a button vs a link, so I smashed them together brutally here. */}
+            <Link to="/pizza" onclick={routeToPizza}>
+                Order Pizza Now!
+            </Link>
+            <img src="https://source.unsplash.com/afDu-GuxjjM" alt="Pizza delivery scooter"></img>
         </div>
     )
 }
