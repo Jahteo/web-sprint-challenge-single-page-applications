b015040f2b65f957d6a093e4aafb12301a587267
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-21 08:38:26 -0700

    Home & NavBar wired up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import NavBar from "./components/NavBar"
+import Home from "./components/Home"
 
 const App = () => {
   return (
     <>
+      <NavBar />
+      <Home />
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
     </>
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Home () {
+
+    return (
+        <div>
+            Pizza Button
+            {/* <img src="../assets/pizza_delivery_scooter.img" alt="Pizza delivery scooter"></img> */}
+        </div>
+    )
+}
